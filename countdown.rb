025e1fa9a5c26eb4293f26c64cbb2fa2feecda70
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-10 00:59:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 #write your code here
 
 def countdown(integer)
-  number= integer 
+  number= integer
   while i < integer
-    
+
     puts "#{number} SECOND(S)!"
-    number -= 1 
+    number -= 1
   end
   "HAPPY NEW YEAR!"
 end
